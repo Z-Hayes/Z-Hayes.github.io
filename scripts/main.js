e667f5b603a92c396577b3f9a9a8dbab40d000fa
Zöe Hayes <zoebhayes96@gmail.com>
2018-07-27 12:53:02 +0100

    added image to background

--- scripts/main.js
+++ scripts/main.js
@@ -2,4 +2,8 @@
 //     document.body.style.background = "#f3f3f3 url('img_tree.png') no-repeat right top";
 // }
 
+function setBackground() {
+    document.body.style.backgroundColor = "#f3f3f3";
+    document.body.style.backgroundImage = "url('images/background.png')";
+}
 document.body.style.backgroundColor = "red";
